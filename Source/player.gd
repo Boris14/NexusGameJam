7153b6e8a38d9ba81cf6c0c83194997cb98b2d12
Boris Dimitrov <boris1234dimitrov@gmail.com>
2023-04-23 15:59:00 +0300

    Tweaked the jump and added some properties to the Player

--- Source/player.gd
+++ Source/player.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
 @export var speed = 300.0
-@export var jump_velocity = -600.0
+@export var max_jump_velocity = -800.0
+@export var max_jump_hold_time = 0.2
+@export var max_health = 100.0
+@export var punch_damage = 10.0
+@export var kick_damage = 20.0
+
+# How fast the player reaches it's max_jump_velocity (not changing is recommended)
+const JUMP_FORCE = 20 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var _is_jumping = false;
+var _jump_time = 0
+var _is_jumping = false
+var heatlh = max_health
+
+func _enter_tree():
+	_is_jumping = false
+	_jump_time = 0
+	heatlh = max_health
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,13 +27,17 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if _is_jumping:
-		if Input.is_action_pressed("player_1_jump"):
-			velocity.y = lerp(velocity.y, jump_velocity, delta * 100)
-		else:
-			_is_jumping = false
-	elif Input.is_action_just_pressed("player_1_jump") and is_on_floor():
+	if Input.is_action_just_pressed("player_1_jump") and is_on_floor():
 		_is_jumping = true
+	elif Input.is_action_just_released("player_1_jump"):
+		_is_jumping = false
+		_jump_time = 0
+	if (
+		_is_jumping and Input.is_action_pressed("player_1_jump") 
+		and _jump_time < max_jump_hold_time
+	):
+		velocity.y = lerp(velocity.y, max_jump_velocity, delta * JUMP_FORCE)
+		_jump_time += delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
